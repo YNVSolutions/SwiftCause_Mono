6dab1b26705aaa751ca3cca70c7bf68e7d7592bc
Yuvraj Shishodia <yuvrajshishodia24@gmail.com>
2026-04-14 16:43:39 +0530

    feat(config): add exportKiosks function URL mapping

--- src/shared/config/functions.ts
+++ src/shared/config/functions.ts
@@ -20,6 +20,7 @@ export const FUNCTION_URLS = {
   exportGiftAidDeclarations: getFunctionUrl('exportGiftAidDeclarations'),
   downloadGiftAidExportBatchFile: getFunctionUrl('downloadGiftAidExportBatchFile'),
   exportDonations: getFunctionUrl('exportDonations'),
+  exportKiosks: getFunctionUrl('exportKiosks'),
   kioskLogin: getFunctionUrl('kioskLogin'),
   createUser: getFunctionUrl('createUser'),
   updateUser: getFunctionUrl('updateUser'),
