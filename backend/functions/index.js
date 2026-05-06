8c4ccf591bfbfaf4c3a9123e1f3114830a8f0c70
Yuvraj Shishodia <yuvrajshishodia24@gmail.com>
2026-05-06 16:31:52 +0530

    feat(gasds): expose batch file download cloud function

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -33,6 +33,7 @@ const {
   exportGiftAidDeclarations,
   downloadGiftAidExportBatchFile,
   exportGasdsCsv,
+  downloadGasdsExportBatchFile,
 } = require('./handlers/giftAid');
 const { exportDonations } = require('./handlers/donationsExport');
 const { exportSubscriptions } = require('./handlers/subscriptionsExport');
@@ -138,6 +139,7 @@ exports.createExpressDashboardLink = functions.https.onRequest(
 exports.exportGiftAidDeclarations = functions.https.onRequest(exportGiftAidDeclarations);
 exports.downloadGiftAidExportBatchFile = functions.https.onRequest(downloadGiftAidExportBatchFile);
 exports.exportGasdsCsv = functions.https.onRequest(exportGasdsCsv);
+exports.downloadGasdsExportBatchFile = functions.https.onRequest(downloadGasdsExportBatchFile);
 exports.exportDonations = functions.https.onRequest(exportDonations);
 exports.exportSubscriptions = functions.https.onRequest(exportSubscriptions);
 exports.exportKiosks = functions.https.onRequest(exportKiosks);
